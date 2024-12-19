bda21561c3023bea8d96d8ff74ecf1cd93335633
Dani <danielius.polujanskis@gmail.com>
2024-12-19 21:57:47 +0200

    fixed bugs no.7,2,3

--- FirstWebApi/Models/DataContext.cs
+++ FirstWebApi/Models/DataContext.cs
@@ -6,5 +6,17 @@ namespace FirstWebApi.Models
     {
         public DbSet<Note> Notes { get; set; }
         public DbSet<Notebook> Notebooks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Note>()
+                    .HasOne(n => n.Notebook)
+                    .WithMany(e => e.Notes)
+                    .HasForeignKey(e => e.NotebookId);
+            modelBuilder.Entity<Notebook>()
+                .HasOne(e => e.Room)
+                .WithMany(e => e.Notebooks)
+                .HasPrincipalKey(e => e.UniqueKey);
+        }
     }
 }
--- FirstWebApi/Models/Notebook.cs
+++ FirstWebApi/Models/Notebook.cs
@@ -1,8 +1,15 @@
-﻿namespace FirstWebApi.Models
+﻿using System.Text.Json.Serialization;
+using Microsoft.Extensions.Hosting;
+
+namespace FirstWebApi.Models
 {
     public class Notebook
     {
         public int Id { get; set; }
-        public string NotebookTitle { get; set; } = "";
+        public string Title { get; set; } = "";
+        public ICollection<Note> Notes { get; set; }
+        public string UniqueKey { get; set; }
+        [JsonIgnore]
+        public Room Room { get; set; }
     }
 }
