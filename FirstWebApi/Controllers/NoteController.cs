4c4f109bbad42817ab0253e42583e0974e1ec35a
Dani <danielius.polujanskis@gmail.com>
2024-12-20 23:02:13 +0200

    Separated controllers, initialized DTO's refactored services

--- FirstWebApi/Controllers/NoteController.cs
+++ FirstWebApi/Controllers/NoteController.cs
@@ -8,17 +8,6 @@ namespace FirstWebApi.Controllers
 {
     public class NoteController : ControllerBase
     {
-        /* TODO: 
-        * 1. ONGOING >>> I need to create some special token(room) which will have notebooks sharable
-        * 2. DONE === If notebook exists do not allow to create new one
-        * 3. DONE === Each note should be easily assigned to notebook without new notebook being created
-        * 4. DONE === Each note should be easy to edit if not entering any values or leaving empty strings
-        * 5. DONE === Throw bad requests instead of new errors
-        * 6. Add AUTH based on token with expiration time ?
-        * 7. DONE === Fix note.Done issue that is note.Done not provided it autofills as false even if value existed
-        * 8. Try to init DTO's for notebook perhaps ? and others if possible
-        * 9. Separate Notebook controllers and Room controllers from Notes controlles
-        */
 
         private readonly DataContext _dataContext;
         private readonly NotesService _notesService;
@@ -35,70 +24,19 @@ namespace FirstWebApi.Controllers
 
         }
 
-        [HttpGet("/api/GetNotebooks")]
-        public ActionResult<IEnumerable<Notebook>> GetNotebooks()
-        {
-            return _dataContext.Notebooks.Include(x => x.Notes).ToList();
-        }
-
-        [HttpPost("/api/CreateNotebook")]
-        public ActionResult CreateNotebook(Notebook notebook)
-        {
-            Notebook existingNotebook = _dataContext.Notebooks.FirstOrDefault(n => n.Id == notebook.Id);
-            Notebook existingNotebookTitle = _dataContext.Notebooks.FirstOrDefault(n => n.Title == notebook.Title);
-            if (existingNotebook == null)
-            {
-                if (notebook.Title == null)
-                {
-                    return BadRequest("Failed to create Notebook. Please enter Title!");
-                }
-                if (existingNotebookTitle != null)
-                {
-                    return BadRequest("Failed to create Notebook. Title already exists!");
-                }
-                else
-                {
-                    _dataContext.Entry(notebook).Reference(x => x.Title);
-                    _dataContext.Entry(notebook).Reference(x => x.Title);
-
-                    //_dataContext.Notebooks.Add(notebook); // old
-                    _dataContext.SaveChanges();
-                }
-            }
-            else
-            {
-                return BadRequest("Failed to create Notebook. Notebook with same Id exists!");
-            }
-            return Ok("Succesfully created note with Id: " + notebook.Id + " and Title: " + notebook.Title);
-        }
-
-        [HttpPost("api/CreateRoom")]
-        public ActionResult CreateRoom()
-        {
-            Room room = new Room();
-            return Ok(room.UniqueKey);
-        }
-
         [HttpPost("/api/CreateNote")]
-        public ActionResult PostNotes(Note note)
+        public async Task<IActionResult> PostNotes(NoteDTO noteDTO)
         {
-            Note existingNote = _dataContext.Notes.FirstOrDefault(n => n.Id == note.Id);
-            var notebook = _dataContext.Notebooks.FindAsync(note.Notebook);
+            try
+            {
+                var createNote = await _notesService.CreateNote(noteDTO);
+                return Ok(createNote);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
 
-            if (existingNote == null)
-            {
-                if (notebook == null)
-                {
-                    return BadRequest("Notebook not found");
-                }
-                _dataContext.Notes.Add(note);
-                _dataContext.SaveChanges();
             }
-            else
-            {
-                return BadRequest("Failed to create Note perhaps note with same ID exists ?");
-            }
-            return Ok("Succesfully created note with Id: " + note.Id + " and Title: " + note.Title);
         }
 
         [HttpPut("/api/EditNote")]
---
+++ FirstWebApi/Controllers/RoomController.cs
@@ -0,0 +1,23 @@
+﻿using FirstWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApi.Controllers
+{
+    public class RoomController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public RoomController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpPost("api/CreateRoom")]
+        public ActionResult CreateRoom()
+        {
+            Room room = new Room();
+            return Ok(room.UniqueKey);
+        }
+
+    }
+}
