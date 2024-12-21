cb3402e01d6c505d8dda7d4acbcd716f6d9458a8
Dani <danielius.polujanskis@gmail.com>
2024-12-21 05:12:05 +0200

    add unit tests solutoin add tests themselves later :D

---
+++ MyApi.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+public class MyApiTests
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        // Arrange
+        var result = 2 + 2;
+
+        // Assert
+        Assert.Equal(4, result);
+    }
+}
