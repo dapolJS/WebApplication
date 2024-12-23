e45ed23360e8ad038ac66186045dc75c82e6b9e1
Dani <danielius.polujanskis@gmail.com>
2024-12-23 23:39:01 +0200

    fixed where filter by title returned only 1 result now returns many

--- FirstWebApi/Controllers/NotebookController.cs
+++ FirstWebApi/Controllers/NotebookController.cs
@@ -17,7 +17,7 @@ namespace FirstWebApi.Controllers
             _notebooksService = notebooksService;
         }
 
-        [HttpGet("/api/Notebook/{Id}/{Title}")]
+        [HttpGet("/api/Notebook/{Id}/{Title}")] 
         public ActionResult<Notebook> Notebook(int Id = 0, string Title = "None")
         {
             if (Id != 0)
@@ -31,7 +31,7 @@ namespace FirstWebApi.Controllers
             }
             else if (Title != "None")
             {
-                var notebook = _dataContext.Notebooks.FirstOrDefault(x => x.Title == Title);
+                var notebook = _dataContext.Notebooks.Where(x => x.Title.Contains(Title));
                 if (notebook == null)
                 {
                     return NotFound("Title not found!");
