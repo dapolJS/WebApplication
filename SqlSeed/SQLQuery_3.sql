e6f2ab0ac7bd52475a67b24f4bb9dca595ef3257
Dani <danielius.polujanskis@gmail.com>
2024-12-31 14:00:01 +0200

    fix sql

--- SqlSeed/SQLQuery_3.sql
+++ SqlSeed/SQLQuery_3.sql
@@ -2,8 +2,17 @@
 USE FirstWebApiNotes; -- your database name
 GO
 
-SELECT * FROM NotesAppSchema.Note
+-- Enable IDENTITY_INSERT to allow explicit values to be inserted into the identity column
+SET IDENTITY_INSERT NotesAppSchema.Note ON;
+GO
 
--- Example of inserting seed data into a table
-INSERT INTO NotesAppSchema.Note (Id, Title, [Description], Done) VALUES ('2', 'SeedTitle', 'SeedDescription', 0);
-INSERT INTO NotesAppSchema.Note (Id, Title, [Description], Done) VALUES ('6', 'SeedTitle', 'SeedDescription', 0);
+-- Insert your seed data with explicit Id values
+INSERT INTO NotesAppSchema.Note (Id, Title, [Description], Done) 
+VALUES (2, 'SeedTitle', 'SeedDescription', 0);
+
+INSERT INTO NotesAppSchema.Note (Id, Title, [Description], Done) 
+VALUES (6, 'SeedTitle', 'SeedDescription', 0);
+
+-- Disable IDENTITY_INSERT after inserting the data
+SET IDENTITY_INSERT NotesAppSchema.Note OFF;
+GO
