1bb06c6e421a0a7101250d7761898d3f4f33eeec
Dani <danielius.polujanskis@gmail.com>
2025-01-03 15:53:07 +0200

    Notebook tests

---
+++ MyApi.Tests/NotesTests/NotebookControllerTests.cs
@@ -0,0 +1,171 @@
+using System.Text;
+using FirstWebApi.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using FirstWebApi.DTOs;
+
+namespace MyApi.Tests.NotebookTests
+{
+    public class NotebookControllerIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+        public NotebookControllerIntegrationTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+        [Fact(DisplayName = " =========== TC1 Returns list of existing Notebooks")]
+        public async Task GetNotesReturnsListOfNotes()
+        {
+            // Act
+            var response = await _client.GetAsync("/api/Notebook/0/None"); // API endpoint to get notes
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Ensure the response code is 200 OK
+
+            var content = await response.Content.ReadAsStringAsync();
+            var notes = JsonConvert.DeserializeObject<List<NotebookDTO>>(content);
+
+            Assert.NotNull(notes);  // Verify the list is not null
+            Assert.NotEmpty(notes);  // Verify that the list is not empty
+            Assert.True(notes.Count >= 1);  // Check if there is more then 8 notes
+        }
+        [Fact(DisplayName = " =========== TC2 Create Notebook with Title, UniqueKey")]
+        public async Task PostNotebookReturnsCreatedNotebook()
+        {
+
+            NotebookDTO notebookDTO = new NotebookDTO // New note object to be created
+            {
+                Title = "PostNotebook",
+                UniqueKey = "Test UniqueKey",
+            };
+            var response = await _client.PostAsync("/api/CreateNotebook", new StringContent(JsonConvert.SerializeObject(notebookDTO), Encoding.UTF8, "application/json"));
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            response.EnsureSuccessStatusCode(); // Ensure the response code is 200 OK
+
+            // Assert
+            Console.WriteLine(" ===> PostNotebookReturnsCreatedNotebook Response body : " + content);
+            Notebook jsonContent = JsonConvert.DeserializeObject<Notebook>(content);
+            Assert.NotEmpty(content);
+            Assert.NotNull(jsonContent.Title);
+            Assert.NotNull(jsonContent.UniqueKey);
+            Assert.Equal(jsonContent.UniqueKey, notebookDTO.UniqueKey);
+
+        }
+        [Fact(DisplayName = " =========== TC3 Create Notebook with empty title")]
+        public async Task PostNotebookReturnsBadRequestWithoutTitle()
+        {
+            NotebookDTO notebookDTO = new NotebookDTO
+            {
+                Title = "",
+                UniqueKey = "From PostNotes test, Title not existing"
+            };
+            var response = await _client.PostAsync("/api/CreateNotebook", new StringContent(
+                JsonConvert.SerializeObject(notebookDTO), Encoding.UTF8, "application/json"));
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Console.WriteLine(" ===> Response body : " + content);
+            Assert.NotEmpty(content);
+            Assert.Equal("Failed to create Notebook. Please enter Title!", content);
+        }
+        [Fact(DisplayName = " =========== TC4 Create Notebook with empty UniqueKey")]
+        public async Task PostNotebookReturnOkWithoutUniqueKey()
+        {
+            NotebookDTO notebookDTO = new NotebookDTO
+            {
+                Title = "PostNotesTitle",
+                UniqueKey = ""
+            };
+
+            var response = await _client.PostAsync("/api/CreateNotebook", new StringContent(
+                JsonConvert.SerializeObject(notebookDTO), Encoding.UTF8, "application/json"));
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            response.EnsureSuccessStatusCode(); // Ensure the response code is 200 OK
+
+            // Assert
+            Console.WriteLine(" ===> PostNotebookReturnOkWithoutUniqueKey Response body : " + content);
+            Notebook jsonContent = JsonConvert.DeserializeObject<Notebook>(content);
+            Assert.NotEmpty(content);
+            Assert.Equal(jsonContent.Title, jsonContent.Title);
+            Assert.Equal(jsonContent.UniqueKey, notebookDTO.UniqueKey);
+        }
+
+        [Fact(DisplayName = " =========== TC5 Createbook with note")]
+        public async Task PostNotebookCreateNotebookWithExistingNote()
+        {
+            Note note = new Note
+            {
+                Title = "TestNotebook",
+                Description = "For Notebook test"
+            };
+
+            Notebook notebook = new Notebook
+            {
+                Notes = [note],
+                Title = "Test Title",
+                UniqueKey = "Test UniqueKey",
+            };
+
+            var response = await _client.PostAsync("/api/CreateNotebook", new StringContent(
+                JsonConvert.SerializeObject(notebook), Encoding.UTF8, "application/json"));
+
+            response.EnsureSuccessStatusCode(); // Ensure the response code is 200 OK
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            NotebookDTO jsonContent = JsonConvert.DeserializeObject<NotebookDTO>(content);
+            Console.WriteLine(" ===> PostNotebookCreateNotebookWithExistingNote Response body : " + content);
+            Assert.NotEmpty(content);
+            Assert.Equal(notebook.Title, jsonContent.Title);
+            Assert.Equal(notebook.UniqueKey, jsonContent.UniqueKey);
+            Assert.True(notebook.Notes.Count >= 1);
+
+        }
+        [Fact(DisplayName = " =========== TC6 Delete existing Notebook by Id")]
+        public async Task DeleteNotebookById()
+        {
+
+            Notebook notebookToBeDeleted = new Notebook // New note object to be created
+            {
+                Title = "PostNotes",
+                UniqueKey = "This is from Integration test",
+            };
+            // Creating new note to delete in this test
+            var responseToBeDeletedNote = await _client.PostAsync("/api/CreateNotebook", new StringContent(JsonConvert.SerializeObject(notebookToBeDeleted), Encoding.UTF8, "application/json"));
+            var contentToBeDeleted = await responseToBeDeletedNote.Content.ReadAsStringAsync();
+            responseToBeDeletedNote.EnsureSuccessStatusCode(); // Ensure the response code is 200 OK
+
+            Console.WriteLine(" ===> contentToBeDeleted Response body : " + contentToBeDeleted);
+            Notebook jsonContent = JsonConvert.DeserializeObject<Notebook>(contentToBeDeleted);
+
+            int noteId = jsonContent.Id;
+
+            var response = await _client.DeleteAsync($"/api/DeleteNotebook/{noteId}");
+            response.EnsureSuccessStatusCode(); // Ensure the response code is 200 OK
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(" ===> DeleteNotebookById Response body : " + content);
+            Assert.NotEmpty(content);
+            Assert.Contains("Succesfully deleted Notebook", content);
+        }
+        [Fact(DisplayName = " =========== TC7 Delete none existing Notebook by Id")]
+        public async Task DeleteNoneExistingNotebookById()
+        {
+            int notebookId = 09128380;
+
+            var response = await _client.DeleteAsync($"/api/DeleteNotebook/{notebookId}");
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(" ===> DeleteNoneExistingNotebookById Response body : " + content);
+            Assert.NotEmpty(content);
+            Assert.Contains($"Notebook by Id : {notebookId} not found!", content);
+        }
+    }
+}
