99f9a9c513ab9e8bc040318a29d00f5fd4638052
Danielius Polujanskis <dapol@danskebank.lt>
2025-03-16 16:32:42 +0200

    add first auth try with inmemory db

--- FirstWebApi/Program.cs
+++ FirstWebApi/Program.cs
@@ -2,6 +2,8 @@ using FirstWebApi.Services;
 using FirstWebApi.Models;
 using Microsoft.EntityFrameworkCore;
 using SwaggerThemes;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,10 @@ builder.Services.AddControllers();
 // builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase(databaseName: "NotesList")); // When you dont want to setup database use this .net in memory functionality
 // Use SQL Server for DataContextEF
 builder.Services.AddDbContext<DataContextEF>(options => options.UseSqlServer(connectionString)); // Use the connection string name from appsettings.json
+builder.Services.AddDbContext<IdentityDbContext>(options => options.UseInMemoryDatabase("AppDb")); // TODO: Use in memory for identity, how to use local db ?
+builder.Services.AddIdentityCore<IdentityUser>().AddEntityFrameworkStores<IdentityDbContext>().AddApiEndpoints();
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddAuthorizationBuilder();
 
 Console.WriteLine(connectionString);
 Console.WriteLine("Environment : " + builder.Environment.EnvironmentName);
@@ -40,6 +46,7 @@ else
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapIdentityApi<IdentityUser>();
 
 app.Run();
 public partial class Program { }
