1110438b44ea69c4ce3cc3f687721502c2f3edcf
Danielius Polujanskis <dapol@danskebank.lt>
2025-04-01 11:38:11 +0300

    update migrations

--- FirstWebApi/Models/Note.cs
+++ FirstWebApi/Models/Note.cs
@@ -6,10 +6,10 @@ public class Note
 {
     public int Id { get; init; }
     public int? NotebookId { get; set; }
-    public DateTime? Date { get; init; }
     public string Title { get; set; } = "";
     public string Description { get; set; } = "";
     public bool? Done { get; set; }
+    public DateTime? Date { get; init; }
 
     [JsonIgnore]
     public Notebook? Notebook { get; init; } // Navigation property
